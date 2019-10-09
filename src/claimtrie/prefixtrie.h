845e80fa178f767b411fad21c1f827c081d20866
Anthony Fieroni <bvbfan@abv.bg>
2019-10-09 18:09:30 +0300

    Introduce libclaimtrie
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- src/claimtrie/prefixtrie.h
+++ src/claimtrie/prefixtrie.h
@@ -1,5 +1,5 @@
-#ifndef BITCOIN_PREFIXTRIE_H
-#define BITCOIN_PREFIXTRIE_H
+#ifndef CLAIMTRIE_PREFIXTRIE_H
+#define CLAIMTRIE_PREFIXTRIE_H
 
 #include <algorithm>
 #include <functional>
@@ -10,8 +10,22 @@
 #include <vector>
 
 #include <boost/container/flat_map.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 
 namespace bc = boost::container;
+namespace fs = boost::filesystem;
+
+class CMemFileConf
+{
+public:
+    fs::path fileName;
+    uint32_t fileSize = 0;
+
+    static CMemFileConf& global();
+private:
+    CMemFileConf() = default;
+};
 
 template <typename TKey, typename TData>
 class CPrefixTrie
@@ -222,4 +236,4 @@ static std::size_t match(const TKey& a, const TKey& b)
     return count;
 }
 
-#endif // BITCOIN_PREFIXTRIE_H
+#endif // CLAIMTRIE_PREFIXTRIE_H
